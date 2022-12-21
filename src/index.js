2cfd0163f611a97390aadfe9cc036730b6aaf4ce
Nwobu Chidiebele .D <65330870+Debeson@users.noreply.github.com>
2022-12-21 08:01:58 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Heading() {
+  return <h1>My Favourite Food</h1>;
+}
+ReactDOM.render(
+  <div>
+    <Heading />
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
